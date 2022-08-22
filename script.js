4b7c53ce98ff2d10f5a76636538d57373e879012
rpw02 <s10222930@connect.np.edu.sg>
2022-08-23 07:23:16 +0800

    Lottie animation

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets9.https://assets8.lottiefiles.com/dotlotties/dlf10_aFS9yjD5Yj.lottie.com/packages/lf20_5n8yfkac.json' // lottie file path
+  })
